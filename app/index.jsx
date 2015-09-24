418caf281fa91f0164a582f8058dee0670e68232
Chad Ostrowski <hi@chadoh.com>
2015-09-23 22:05:25 -0400

    Persist entire app state in localStorage

--- app/index.jsx
+++ app/index.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import App from './components/App'
 import './main.css'
 import 'array.prototype.findindex'
+import alt from './libs/alt'
+import storage from './libs/storage'
+import persist from './libs/persist'
 
 main()
 
 function main() {
+  persist(alt, storage(localStorage), 'app')
   const app = document.createElement('div')
   document.body.appendChild(app)
   React.render(<App/>, app)
