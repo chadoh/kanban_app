1cdf4465c7094694f1af246cb5f6f96039b005b0
Chad Ostrowski <hi@chadoh.com>
2015-09-23 20:56:08 -0400

    Implement Flux architecture with Alt

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,30 +1,26 @@
 import uuid from 'node-uuid'
 import React from 'react'
 import Notes from './Notes'
+import NoteStore from '../stores/NoteStore'
+import NoteActions from '../actions/NoteActions'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do Laundry'
-        }
-      ]
-    }
+    this.storeChanged = this.storeChanged.bind(this)
+    this.state = NoteStore.getState()
+  }
 
-    this.addNote = this.addNote.bind(this)
-    this.editNote = this.editNote.bind(this)
-    this.deleteNote = this.deleteNote.bind(this)
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged)
+  }
+
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged)
+  }
+
+  storeChanged(state) {
+    this.setState(state)
   }
 
   render() {
@@ -41,20 +37,16 @@ export default class App extends React.Component {
     )
   }
 
-  editNote(noteId, task) {
-    let notes = this.state.notes;
-    const noteIndex = this.findNote(noteId)
-    if (noteIndex < 0) return;
-    notes[noteIndex].task = task;
-    this.setState({notes: notes})
+  addNote() {
+    NoteActions.create({task: 'New task'})
   }
 
-  deleteNote(noteId) {
-    let notes = this.state.notes;
-    const noteIndex = this.findNote(noteId)
-    if (noteIndex < 0) return;
-    notes.splice(noteIndex, 1)
-    this.setState({notes: notes})
+  editNote(id, task) {
+    NoteActions.update({id, task})
+  }
+
+  deleteNote(id) {
+    NoteActions.delete(id)
   }
 
   findNote(id) {
@@ -66,12 +58,4 @@ export default class App extends React.Component {
     return noteIndex
   }
 
-  addNote() {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    })
-  }
 }
