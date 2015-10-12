05082704d250ac5179439acb848e437efc1b8d52
Chad Ostrowski <hi@chadoh.com>
2015-10-12 09:47:25 -0400

    7.4 Developing onMove api for Notes

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -20,12 +20,16 @@ export default class Notes extends React.Component {
   }
   renderNote(note) {
     return (
-      <Note className="note" id={note.id} key={`note${note.id}`}>
+      <Note className="note" onMove={this.onMoveNote}
+        id={note.id} key={`note${note.id}`}>
         <Editable value={note.task}
           onEdit={this.props.onEdit.bind(null, note.id)}
           onDelete={this.props.onDelete.bind(null, note.id)}
         />
       </Note>
     )
+  }
+  onMoveNote({sourceId, targetId}) {
+    console.log('source', sourceId, 'target', targetId)
   }
 }
