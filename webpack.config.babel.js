fecd8aae143df983a8963919653e70c466c89e86
Chad Ostrowski <hi@chadoh.com>
2015-10-30 11:23:22 -0400

    8.6 Add stats script
    I don't really understand how to read or use these stats. I get sort of
    overwhelmed by the amount of information. I need to dig in more slowly,
    I fear.
    
    So much to learn!

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -61,7 +61,7 @@ if (TARGET === 'start' || !TARGET) {
   })
 }
 
-if (TARGET === 'build') {
+if (TARGET === 'build' || TARGET === 'stats') {
   module.exports = merge(common, {
     entry: {
       app: APP_PATH,
