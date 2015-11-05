ec4909b49ba44692ba9749b715a823234c9dff07
Chad Ostrowski <hi@chadoh.com>
2015-11-05 16:37:38 -0500

    Update ReactDnD to match the book
    And fix a style I broke in a recent commit

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,7 +5,7 @@ import Lanes from './Lanes'
 import LaneStore from '../stores/LaneStore'
 import LaneActions from '../actions/LaneActions'
 import {DragDropContext} from 'react-dnd'
-import HTML5Backend from 'react-dnd/modules/backends/HTML5'
+import HTML5Backend from 'react-dnd-html5-backend'
 
 @DragDropContext(HTML5Backend)
 export default class App extends React.Component {
